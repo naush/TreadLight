6a13ca7516379b5f88737718216e737b4a337386
naush <shuchung1985@gmail.com>
2010-05-05 17:46:46 -0500

    add controller back

---
+++ production/lib/controller.rb
@@ -0,0 +1,68 @@
+$: << File.expand_path(File.dirname(__FILE__) + "/lib")
+require 'constants'
+module Controller
+  def speed=(speed)
+    new_speed = Sanitizer.sanitize_speed(speed.to_f)
+    new_speed == 0.0 ? stop_timer : start_timer
+    @meter.speed = new_speed
+  end
+
+  def speed?
+    return @meter.speed?
+  end
+
+  def incline=(incline)
+    incline_value = incline.chop.to_f
+    @meter.incline = "%0.1f%" % Sanitizer.sanitize_incline(incline_value)
+  end
+
+  def incline?
+    return @meter.incline?
+  end
+
+  def speed_up
+    start_timer
+    new_speed = @meter.speed?.to_f + Constants::SPEED_STEP
+    @meter.speed = Sanitizer.sanitize_speed(new_speed, :up)
+  end
+
+  def speed_down
+    new_speed = Sanitizer.sanitize_speed(@meter.speed?.to_f - Constants::SPEED_STEP, :down)
+    stop_timer if new_speed <= 0.0
+    @meter.speed = new_speed
+  end
+
+  def incline_up
+    @meter.incline = Sanitizer.sanitize_incline(@meter.incline?.to_f + Constants::INCLINE_STEP).to_s + '%'
+  end
+
+  def incline_down
+    @meter.incline = Sanitizer.sanitize_incline(@meter.incline?.to_f - Constants::INCLINE_STEP).to_s + '%'
+  end
+
+  def reset_incline
+    @meter.incline = "1.5%"
+  end
+
+  def start_timer
+    @timer.start
+  end
+
+  def stop_timer
+    @timer.stop
+
+  end
+
+  def reset_timer
+
+    @timer.reset
+
+  end
+
+
+  def time=(times)
+    @timer.time = times
+    @timer.run_task
+  end
+
+end
